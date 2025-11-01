eeded08d6d8acaf36dfebdb89e85c64b9fd64a51
arya012-maker <aryamann233@gmail.com>
2025-11-02 00:11:55 +0530

    Setup.py file ready

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
 from setuptools import setup, find_packages
+from typing import List
+
+
+
+def get_requirements() -> List[str]:
+    """
+    this function will always return a list of requirements
+    """
+    requirement_lst: List[str] = []
+    try:
+        with open("requirements.txt", "r") as file:
+            lines =  file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found. No dependencies will be installed.")
+    
+    return requirement_lst
+
+setup(
+    name="My_Network_Security_Project",
+    version="0.0.1",
+    author="Aryamann",
+    author_email="aryamann233@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)    
